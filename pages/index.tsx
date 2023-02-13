dffeeb88798e0c13f766165c62327254277cb205
Asad Ali Khan WL1 <asad.khan@cooperativecomputing.com>
2023-02-13 13:16:23 +0500

    turned FE to BE

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,86 +27,89 @@ type Props = {
 };
 const Home = ({ pageInfo, experiences, skills, projects, social }: Props) => {
   return (
-    <div className="">
-      <Head>
-        <title>Asad Khan Portfolio</title>
-        <meta
-          name="description"
-          content="Asad Ali Khan is A professional MERN stack Developer with extensive knowledge about Tailwind css and firebase and worked with ANT Design and material UI"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main
-        className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-30 
-      scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80"
-      >
-        <Header social={[social]} />
+    // <div className="">
+    //   <Head>
+    //     <title>Asad Khan Portfolio</title>
+    //     <meta
+    //       name="description"
+    //       content="Asad Ali Khan is A professional MERN stack Developer with extensive knowledge about Tailwind css and firebase and worked with ANT Design and material UI"
+    //     />
+    //     <link rel="icon" href="/favicon.ico" />
+    //   </Head>
+    //   <main
+    //     className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-30
+    //   scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80"
+    //   >
+    //     <Header social={[social]} />
 
-        {/* hero */}
-        <section id="hero" className="snap-start ">
-          {pageInfo?._id && <Hero pageInfo={pageInfo} />}
-        </section>
+    //     {/* hero */}
+    //     <section id="hero" className="snap-start ">
+    //       {pageInfo?._id && <Hero pageInfo={pageInfo} />}
+    //     </section>
 
-        {/* About */}
-        <section id="About" className="snap-center">
-          <About pageInfo={pageInfo} />
-        </section>
-        {/* #Experience*/}
+    //     {/* About */}
+    //     <section id="About" className="snap-center">
+    //       <About pageInfo={pageInfo} />
+    //     </section>
+    //     {/* #Experience*/}
 
-        <section id="Experience" className="snap-center">
-          <ExperienceCards experiences={experiences} />
-        </section>
-        {/* Skills */}
-        <section id="Skills" className="snap-center">
-          <Skills skills={skills} />
-        </section>
-        {/* Projects */}
-        <section id="Projects" className="snap-center">
-          <Projects projects={projects} />
-        </section>
-        {/* Contact || Contact me */}
-        <section id="Contact" className="snap-center">
-          <ContactSection />
-        </section>
-      </main>
+    //     <section id="Experience" className="snap-center">
+    //       <ExperienceCards experiences={experiences} />
+    //     </section>
+    //     {/* Skills */}
+    //     <section id="Skills" className="snap-center">
+    //       <Skills skills={skills} />
+    //     </section>
+    //     {/* Projects */}
+    //     <section id="Projects" className="snap-center">
+    //       <Projects projects={projects} />
+    //     </section>
+    //     {/* Contact || Contact me */}
+    //     <section id="Contact" className="snap-center">
+    //       <ContactSection />
+    //     </section>
+    //   </main>
 
-      <footer className="fixed bottom-0 right-1/2 cursor-pointer  translate-x-1/2 ">
-        <div className=" w-full mx-auto">
-          <Link href="#hero">
-            <div className="flex items-center">
-              <span>Powered by Asad Enterprise</span>
-              <span className="">
-                <Image
-                  src={mainLogo}
-                  alt="Asad Logo"
-                  width={50}
-                  height={50}
-                  // className="object-contain"
-                />
-              </span>
-            </div>
-          </Link>
-        </div>
-      </footer>
-    </div>
+    //   <footer className="fixed bottom-0 right-1/2 cursor-pointer  translate-x-1/2 ">
+    //     <div className=" w-full mx-auto">
+    //       <Link href="#hero">
+    //         <div className="flex items-center">
+    //           <span>Powered by Asad Enterprise</span>
+    //           <span className="">
+    //             <Image
+    //               src={mainLogo}
+    //               alt="Asad Logo"
+    //               width={50}
+    //               height={50}
+    //               // className="object-contain"
+    //             />
+    //           </span>
+    //         </div>
+    //       </Link>
+    //     </div>
+    //   </footer>
+    // </div>
+    <></>
   );
 };
 
 export default Home;
 
 export const getStaticProps: any = async () => {
-  const pageInfo: PageInfo = await fetchPageInfos();
-  const experiences: Experience[] = await fetchExperiences();
-  const skills: Skill[] = await fetchSkills();
-  const projects: Project[] = await fetchProjects();
-  const social: Social[] = await fetchSocials();
+  // const pageInfo: PageInfo = await fetchPageInfos();
+  // const experiences: Experience[] = await fetchExperiences();
+  // const skills: Skill[] = await fetchSkills();
+  // const projects: Project[] = await fetchProjects();
+  // const social: Social[] = await fetchSocials();
+  // return {
+  //   props: {
+  //     pageInfo,
+  //     experiences,
+  //     skills,
+  //     projects,
+  //     social,
+  //   },
   return {
-    props: {
-      pageInfo,
-      experiences,
-      skills,
-      projects,
-      social,
-    },
+    props: {},
   } as unknown;
 };
